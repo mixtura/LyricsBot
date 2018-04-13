43b468e6cf8efca6eb6e9a0897fcef9e32fb5670
Danylo Sydorenko <mixfrom9@gmail.com>
2018-04-13 22:55:02 +0300

    Fix formatting

--- LyricsBot.fs
+++ LyricsBot.fs
@@ -12,8 +12,11 @@ module Utils =
     ChatId : Int64;
     SearchQuery: string;
   }
+  
   let serializeObj obj = JsonConvert.SerializeObject obj
+  
   let deserializeObj<'a> str = JsonConvert.DeserializeObject<'a> str
+  
   let tryToOption result = 
     match result with 
     | (true, uri) -> Some uri
@@ -35,32 +38,39 @@ module AnswerToUser =
 
 module ProcessBotUpdate =  
   open Utils  
+  
   let creatUri str = Uri.TryCreate(str, UriKind.Absolute) |> tryToOption
+  
   let extractQueryValueFromUri key (uri : Uri) = 
     uri.Query.Split '&' |>
     Array.map (fun queryPair -> queryPair.Split '=') |>
     Array.tryFind (fun pair -> pair.[0] = key) |>
     Option.bind (fun pair -> Some pair.[1])
+  
   let parseGoogleMusicLink str = 
     creatUri str |> 
     Option.filter (fun uri -> uri.Host.Equals "play.google.com") |>
     Option.bind (extractQueryValueFromUri "t") |>
     Option.bind (fun query -> query.Replace ('_', ' ') |> Some)
+  
   let parseSearchRequest str = 
     str |> 
     parseGoogleMusicLink |> 
     function
       | Some s -> s
       | None -> str
+
   let (|MessageSearchRequest|_|) (u: Update) = 
     if u.Type = UpdateType.Message 
     then Some {
       ChatId = u.Message.Chat.Id; 
       SearchQuery = (parseSearchRequest u.Message.Text) }
     else None
+
   let storeSearchRequest (searchRequests: ICollection<string>) searchRequest =  
       serializeObj searchRequest |> 
       searchRequests.Add
+
   let run(update: Update, searchRequests: ICollection<string>, log: TraceWriter) = 
     log.Info "Process update"
     match update with
