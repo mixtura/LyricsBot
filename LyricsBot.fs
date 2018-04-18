fba8889370fd56d286c3475e294b6404d84dcdb9
Danylo Sydorenko <mixfrom9@gmail.com>
2018-04-18 23:30:19 +0300

    Rework code, implement functionality

--- LyricsBot.fs
+++ LyricsBot.fs
@@ -6,39 +6,50 @@ open Microsoft.Azure.WebJobs.Host
 open Newtonsoft.Json
 open Telegram.Bot.Types
 open Telegram.Bot.Types.Enums
+open System
 
 module Utils = 
   type GetLyricsRequest = {
     Artist: string;
-    Name: string;
+    Track: string;
   }
 
   type SearchLyricsRequest = {
     SearchQuery: string;
   }
 
-  let rec firstSome arg funcs = 
-    match funcs with
-    | x::xs -> Option.orElseWith (fun () -> firstSome arg xs) (x arg)
+  let rec firstSome fs = 
+    match fs with
+    | x::xs -> Option.orElseWith (fun _ -> firstSome xs) (x())
     | [] -> None
 
-  let serializeObj obj = JsonConvert.SerializeObject obj
-  
-  let deserializeObj<'a> str = JsonConvert.DeserializeObject<'a> str
-  
   let tryToOption result = 
     match result with 
     | (true, uri) -> Some uri
     | (false, _) -> None
 
+  let creatUri str = Uri.TryCreate(str, UriKind.Absolute) |> tryToOption
+
+  let extractQueryValueFromUri key (uri : Uri) = 
+    uri.Query.Split [|'&'; '='|] |>
+    Array.chunkBySize 2 |>
+    Array.tryFind (fun pair -> pair.[0] = key) |>
+    Option.bind (fun pair -> Some pair.[1])
+  
+  let serializeObj obj = JsonConvert.SerializeObject obj  
+  let deserializeObj<'a> str = JsonConvert.DeserializeObject<'a> str
+
 module SearchLyrics = 
+  open Utils
+
+module GetLyrics = 
   open Utils
   open LyricsWikiaGrabber
 
-  let run (searchRequestData: string, log: TraceWriter) = 
-    log.Info("Search lyrics started.")
-    let searchRequest = deserializeObj<GetLyricsRequest> searchRequestData
-    let lyrics = findLyrics searchRequest.Artist searchRequest.Name
+  let run (getLyricsReqData: string, log: TraceWriter) = 
+    log.Info("Get lyrics started.")
+    let (getLyricsReq, chatId) = deserializeObj<Tuple<GetLyricsRequest, int>> getLyricsReqData
+    let lyrics = findLyrics getLyricsReq.Artist getLyricsReq.Track
     ()
 
 module AnswerToUser =
@@ -50,27 +61,21 @@ module ProcessBotUpdate =
   let run(update: Update, searchLyricsRequests: ICollection<string>, getLyricsRequests: ICollection<string>, log: TraceWriter) = 
     log.Info "Process bot update started."
 
-    let creatUri str = Uri.TryCreate(str, UriKind.Absolute) |> tryToOption
-  
-    let extractQueryValueFromUri key (uri : Uri) = 
-      uri.Query.Split [|'&'; '='|] |>
-      Array.chunkBySize 2 |>
-      Array.tryFind (fun pair -> pair.[0] = key) |>
-      Option.bind (fun pair -> Some pair.[1])
-    
     let parseGoogleMusicLink str = 
       creatUri str 
       |> Option.filter (fun uri -> uri.Host.Equals "play.google.com") 
       |> Option.bind (extractQueryValueFromUri "t") 
       |> Option.bind (fun query -> query.Replace('_', ' ').Split('-') |> Some) 
       |> function
-        | Some [|artist; name;|] -> Some { Artist = artist; Name = name  }
+        | Some [|artist; name;|] -> Some { Artist = artist; Track = name  }
         | Some _ | None -> None
     
-    let parseApplyMusicLink str = Some { Artist = ""; Name = ""}
+    let parseAppleMusicLink str = Some { Artist = ""; Track = ""}
 
     let parseAsGetRequest str = 
-      firstSome str [parseGoogleMusicLink; parseApplyMusicLink]
+      [parseGoogleMusicLink; parseAppleMusicLink] 
+      |> List.map (fun f -> fun _ -> f str)
+      |> firstSome
 
     let parseAsSearchRequest str =
       Some { SearchQuery = str }
@@ -84,7 +89,8 @@ module ProcessBotUpdate =
       | MessageUpdate(update) ->
       
         let createQueueMessage request = (update.Message.Chat.Id, request) |> serializeObj |> Some
-          
+        let bindArg arg func = fun _ -> func arg
+        
         let getRequest =
           parseAsGetRequest 
           >> Option.bind createQueueMessage 
@@ -94,9 +100,13 @@ module ProcessBotUpdate =
           parseAsSearchRequest 
           >> Option.bind createQueueMessage
           >> Option.map searchLyricsRequests.Add
+          
+        [getRequest; searchRequest]
+          |> List.map (bindArg update.Message.Text)
+          |> firstSome
+          |> function 
+            | Some _ -> log.Info "Process update succeesed." 
+            | None -> log.Error "Process update fail"
 
-        firstSome update.Message.Text [getRequest; searchRequest] |> function 
-          | Some _ -> log.Info "Process update succeesed." 
-          | None -> log.Error "Process update fail"
       | _ -> log.Error "Process update failed"
     ()
--- LyricsWikiaGrabber.fs
+++ LyricsWikiaGrabber.fs
@@ -6,10 +6,10 @@ module LyricsWikiaGrabber =
     type private LyricsWikiaProvider = HtmlProvider<"http://lyrics.wikia.com/Green Day:Holiday">
     let findLyrics artist song = 
 
-        let getLyricsLink artist song = sprintf "http://lyrics.wikia.com/wiki/%s:%s" artist song
+        let getLyricsLink artist track = sprintf "http://lyrics.wikia.com/wiki/%s:%s" artist track
     
-        let loadLyricsPage artist song = 
-            getLyricsLink artist song |>
+        let loadLyricsPage artist track = 
+            getLyricsLink artist track |>
             LyricsWikiaProvider.AsyncLoad
 
         let extractLyrics (page:LyricsWikiaProvider) = 
